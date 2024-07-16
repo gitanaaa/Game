a4b9fdc3f4f2c63e48121672fa35b365599dba61
DOGMA222 <dogma.o092002@gmail.com>
2024-07-16 23:44:40 +0900

    画像ファイルの追加とウィンドウサイズの変更

--- games.py
+++ games.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.locals import *
 import sys
 
-img_bg = pygame.image.load("fild_image.jpg")
+img_bg = pygame.image.load("fild_image2.jpg")
 
 bg_y = 0
 
@@ -11,7 +11,7 @@ def main():
 
     pygame.init()
     pygame.display.set_caption("トロッコゲーム")
-    screen = pygame.display.set_mode((700,500))
+    screen = pygame.display.set_mode((800,400))
     clock = pygame.time.Clock()
 
     while True:
@@ -19,8 +19,8 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        bg_y = (bg_y+1.25)%500
-        screen.blit(img_bg,[0,bg_y-500])
+        bg_y = (bg_y+1.25)%400
+        screen.blit(img_bg,[0,bg_y-400])
         screen.blit(img_bg,[0,bg_y])
 
         pygame.display.update()
